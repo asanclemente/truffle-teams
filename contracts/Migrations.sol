731847c5c0deb2b86820e730c2dcda5bb97e4a31
CSANCLEMENTE <csanclemente@bancodeoccidente.com.co>
2021-03-09 14:01:43 -0500

    se actualiza contrato se dano

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -5,7 +5,7 @@ contract Migrations {
   address public owner = msg.sender;
   uint public last_completed_migration;
 
-  modifier restricted() {
+  modifier restricted {
     require(
       msg.sender == owner,
       "This function is restricted to the contract's owner"
@@ -13,7 +13,7 @@ contract Migrations {
     _;
   }
 
-  function setCompleted(uint completed) public restricted {
+   setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
   }
 }
